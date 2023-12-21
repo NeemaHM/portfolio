0bf2576fcee64dc25c7ae1125751bdd24f4a66fc
Neema Mwansembo <neyharry2003@gmail.com>
2023-12-21 00:27:25 -0500

    done

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,27 @@
+import pro1 from "../assets/project1.jpg";
+import pro2 from "../assets/project2.jpg";
+import pro3 from "../assets/project3.jpg";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Digital Recipe Book",
+    text: "Cook effortlessly with our digital recipe book. Discover modern and local recipes, carefully chosen for a delightful culinary journey",
+    view: "https://unsplash.com/photos/five-gray-spoons-filled-with-assorted-color-powders-near-chilli-vA1L1jRTM70"
+  },
+  {
+    imgsrc: pro2,
+    title: "E-Commerce and Online Shopping",
+    text: "Discover beauty essentials. Find perfumes, skincare, and makeup. Elevate your routine with our carefully chosen collection for a glowing you",
+    view: "https://unsplash.com/photos/five-gray-spoons-filled-with-assorted-color-powders-near-chilli-vA1L1jRTM70"
+  },
+  {
+    imgsrc: pro3,
+    title: "Weather app",
+    text: "Experience weather like never before. This app offers real-time updates, precise forecasts, and a user-friendly interface.",
+    view: "https://unsplash.com/photos/five-gray-spoons-filled-with-assorted-color-powders-near-chilli-vA1L1jRTM70"
+  },
+];
+
+
+export default ProjectCardData;
