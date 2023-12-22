bfee99edb9e11f1cfc4437d96ae28d6eaca6dbcc
Neema Mwansembo <neyharry2003@gmail.com>
2023-12-22 11:11:14 -0500

    modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,37 @@
 import "./FormStyles.css";
-
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_10i6wsq', 'template_p1e5rr6', form.current, 'Rlsdi5Gbkg7jEoXEuoYcP')
+      .then((result) => {
+        console.log(result.text);
+        alert('Email Sent!');
+        form.current.reset(); // Reset the form
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div className="form">
-      <form>
+      {/* Move the onSubmit handler to the <form> element */}
+      <form ref={form} onSubmit={sendEmail}>
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" name="your_name" />
         <label>Email</label>
-        <input type="email"></input>
+        <input type="email" name="your_email" />
         <label>Subject</label>
-        <input type="text"></input>
+        <input type="text" name="your_subject" />
         <label>Your message</label>
-        <textarea rows="6" placeholder="Type your message here"/>
-        <button className="btn">Submit</button>
+        <textarea rows="6" placeholder="Type your message here" name="your_message" />
+        {/* Adjust the type attribute to submit */}
+        <button className="btn" type="submit">Submit</button>
       </form>
     </div>
   );
